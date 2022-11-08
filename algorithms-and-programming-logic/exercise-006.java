73a29765fd2675b88872c0820790af132bc2a32a
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2022-11-08 10:49:27 -0300

    Create exercise-006.java

---
+++ algorithms-and-programming-logic/exercise-006.java
@@ -0,0 +1,18 @@
+package javaapplication1;
+import java.util.Scanner;
+public class exercise-006 {
+    public static void main(String[] args) {
+        int e[] = new int[15];
+        Scanner in = new Scanner(System.in);
+        
+        System.out.println("Digite 15 números inteiros: ");
+        
+        System.out.print("Os valores pares são: ");
+        
+        for(int i = 0; i < e.length; i++) {
+            if (e[i] % 2 == 0) {
+                System.out.print("O " + (i + 1) + "º elemento do vetor, que vale: " + e[i] + ".");
+            }
+        }
+    } 
+}
