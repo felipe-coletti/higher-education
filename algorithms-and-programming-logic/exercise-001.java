9e9ad9f0731a88b9f837826eadb144290c788130
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2022-11-08 10:55:26 -0300

    Update exercise-001.java

--- algorithms-and-programming-logic/exercise-001.java
+++ algorithms-and-programming-logic/exercise-001.java
@@ -2,16 +2,18 @@ package javaapplication1;
 import java.util.Scanner;
 public class exercise-001 {
     public static void main(String[] args) {
-        float n[] = new float[10];
+        float n[] = new float[9];
         Scanner in = new Scanner(System.in);
         
         System.out.println("Digite as 10 notas: ");
         
         for(int i = 0; i < n.length; i++) {
             n[i] = in.nextFloat();
-            
-            if (i == 7 || i == 9) {
-                System.out.print("A " + (i + 1) + "º nota é: " + n[i] + ".");
+        }
+        
+        for(int i = 0; i < n.length; i++) {
+            if (i == 6 || i == 8) {
+                System.out.print("A " + (i + 1) + "º nota é " + n[i] + ".");
             }
         }
     }
