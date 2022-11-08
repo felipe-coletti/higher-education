cf85ec4550c5aeb8ac5ae3fc4707a299e7bd37b7
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2022-11-08 10:18:20 -0300

    Update exercise-001.java

--- algorithms-and-programming-logic/exercise-001.java
+++ algorithms-and-programming-logic/exercise-001.java
@@ -1,6 +1,6 @@
 package javaapplication1;
 import java.util.Scanner;
-public class JavaApplication1 {
+public class exercise-001 {
     public static void main(String[] args) {
         float n[] = new float[10];
         Scanner in = new Scanner(System.in);
