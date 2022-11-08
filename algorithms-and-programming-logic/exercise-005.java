738a583b50db506864b7a791f335a2bc18fbce6f
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2022-11-08 10:37:44 -0300

    Update exercise-005.java

--- algorithms-and-programming-logic/exercise-005.java
+++ algorithms-and-programming-logic/exercise-005.java
@@ -9,12 +9,12 @@ public class exercise-005 {
             System.out.print("O " + (i + 1) + "º número do vetor A é: " + a[i] + ".");
         }
         
-        for(int i = 0; i < a.length; i++) {
+        for(int i = 0; i < b.length; i++) {
             System.out.print("O " + (i + 1) + "º número do vetor B é: " + b[i] + ".");
+            c[i] = (a[i] * b[i]);
         }
         
-        for(int i = 0; i < a.length; i++) {
-            c[i] = (a[i] * b[i]);
+        for(int i = 0; i < c.length; i++) {
             System.out.print("O " + (i + 1) + "º número do vetor C é: " + c[i] + ".");
         }
     } 
