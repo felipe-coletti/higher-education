da167c845c25c41cc083f14bec6b01eba032d600
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-02-15 10:16:56 -0300

    Update fonte.js

--- web-development/module-001/website/js/fonte.js
+++ web-development/module-001/website/js/fonte.js
@@ -1,12 +1,12 @@
-$(document).ready(function(){
+$(document).ready(function() {
     let fonte = 14
-    $("#aumentar").click(function(){
+    $("#aumentar").click(function() {
         if(fonte < 22){
             $("body").css({"font-size": ++fonte + "px"})
         }
     })
-    $("#diminuir").click(function(){
-        if(fonte > 6){
+    $("#diminuir").click(function() {
+        if(fonte > 6) {
             $("body").css({"font-size": --fonte + "px"})
         }
     })
