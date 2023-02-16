11961f7ed10449eddea9386dd2c72023b6fe5e27
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-02-16 08:47:45 -0300

    Create exercise-001.java

---
+++ data-structure/module-001/exercise-001.java
@@ -0,0 +1,11 @@
+package javaapplication1;
+
+public class JavaApplication1 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
