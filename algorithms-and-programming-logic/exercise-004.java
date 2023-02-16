429e8e3ef20c95763c23cedf18ac82ec0cc475fa
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-02-16 09:28:54 -0300

    Update exercise-004.java

--- algorithms-and-programming-logic/exercise-004.java
+++ algorithms-and-programming-logic/exercise-004.java
@@ -2,15 +2,15 @@ package javaapplication1;
 
 public class JavaApplication1 {
     public static void main(String[] args) {
-        float numeros[] = {3, 5, 6, 8, 10};
-        float soma = 0;
+        float numbers[] = {3, 5, 6, 8, 10};
+        float sum = 0;
         
         for(int i = 0; i < numeros.length; i++) {
-            if (numeros[i] % 2 != 0) {
-                soma += numeros[i];
+            if (numbers[i] % 2 != 0) {
+                sum += numbers[i];
             }
             
-            System.out.print("A soma dos números impares é: " + soma + ".");
+            System.out.print("A soma dos números impares é: " + sum + ".");
         }
     } 
 }
