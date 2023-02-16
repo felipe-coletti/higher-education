d27d5c78d800889a5a39d238947bc9223bb6fb68
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-02-16 09:20:13 -0300

    Update exercise-002.java

--- algorithms-and-programming-logic/exercise-002.java
+++ algorithms-and-programming-logic/exercise-002.java
@@ -4,24 +4,24 @@ import java.util.Scanner;
 
 public class JavaApplication1 {
     public static void main(String[] args) {
-        float n[] = new float[10];
-        float a = 0;
+        float notas[] = new float[10];
+        float aprovados = 0;
         Scanner in = new Scanner(System.in);
         
         System.out.println("Digite as 10 notas: ");
         
-        for(int i = 0; i < n.length; i++) {
-            n[i] = in.nextFloat();
+        for(int i = 0; i < notas.length; i++) {
+            notas[i] = in.nextFloat();
         }
         
-        for(int i = 0; i < n.length; i++) {
-            System.out.print("A nota do" + (i + 1) + "º aluno é: " + n[i] + ".");
+        for(int i = 0; i < notas.length; i++) {
+            System.out.print("A nota do" + (i + 1) + "º aluno é: " + notas[i] + ".");
             if (i > 7) {
-                a++;
+                aprovados++;
             }
         }
         
-        System.out.print("A quantidade de alunos aprovados é: " + a + ".");
-        System.out.print("A quantidade de alunos reprovados é: " + (n.length - a) + ".");
+        System.out.print("A quantidade de alunos aprovados é: " + aprovados + ".");
+        System.out.print("A quantidade de alunos reprovados é: " + (notas.length - aprovados) + ".");
     } 
 }
