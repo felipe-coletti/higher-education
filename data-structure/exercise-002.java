7f904616bc930689afe11e7330e9db0fed86b61d
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-02-16 10:12:20 -0300

    Update exercise-002.java

--- data-structure/exercise-002.java
+++ data-structure/exercise-002.java
@@ -3,10 +3,11 @@ package javaapplication1;
 public class JavaApplication1 {
     public static void main(String[] args) {
         int number = 6;
+        int result = number;
         
         for (int i = number - 1; i >= 1; i--) {
-            int result = number * i;
-            System.out.println(number + " x " + i + " = " + result);
+            result *= i;
+            System.out.println(result + " x " + i + " = " + result);
         }
     }
 }
