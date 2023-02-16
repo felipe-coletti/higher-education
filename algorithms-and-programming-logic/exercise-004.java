dc660b1efde40386b6a80ab445f42896734e9221
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-02-16 08:51:27 -0300

    Update exercise-004.java

--- algorithms-and-programming-logic/exercise-004.java
+++ algorithms-and-programming-logic/exercise-004.java
@@ -1,5 +1,6 @@
 package javaapplication1;
-public class exercise-004 {
+
+public class JavaApplication1 {
     public static void main(String[] args) {
         float a[] = {3, 5, 6, 8, 10};
         float s = 0;
