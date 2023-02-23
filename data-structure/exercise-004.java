689a88a915a97f7351baef2ce201dfe175e6a4f4
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-02-23 10:27:53 -0300

    Update exercise-004.java

--- data-structure/exercise-004.java
+++ data-structure/exercise-004.java
@@ -2,10 +2,9 @@ package javaapplication1;
 
 public class JavaApplication1 {
     public static void main(String[] args) {
-        String result = null;
-        
         for (int line = 1; line <= 10; line++) {
-            for (int column = 1; column <= 10; column += line) {
+            String result = "";
+            for (int column = 1 * line; column <= 10 * line; column += line) {
                 result += (column + " | ");
             }
             System.out.println(result);
