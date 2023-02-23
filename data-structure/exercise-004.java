d4172dfc93b89953007b8e253f33775fea1851d7
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-02-23 10:38:38 -0300

    Update exercise-004.java

--- data-structure/exercise-004.java
+++ data-structure/exercise-004.java
@@ -4,9 +4,11 @@ public class JavaApplication1 {
     public static void main(String[] args) {
         for (int line = 1; line <= 10; line++) {
             String result = "";
+            
             for (int column = 1 * line; column <= 10 * line; column += line) {
                 result += (column + " | ");
             }
+            
             System.out.println(result);
         }
     }
