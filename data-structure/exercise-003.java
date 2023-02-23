1770726a9d676aae7f67f4b6d4478773949a6dc1
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-02-23 10:45:06 -0300

    Update exercise-003.java

--- data-structure/exercise-003.java
+++ data-structure/exercise-003.java
@@ -5,13 +5,12 @@ import java.util.Scanner;
 public class JavaApplication1 {
     public static void main(String[] args) {
         Scanner leitor = new Scanner(System.in);
-
-        int number;
+        
         int result = 0;
 
         do {
             System.out.println("Digite um número inteiro: ");
-            number = leitor.nextInt();
+            int number = leitor.nextInt();
             result += number;
             System.out.println("Soma: " + result);
         } while(number != 0);
