a3bf030d3804da63c1274bb823246b4f86e7fed4
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-02-28 10:11:58 -0300

    Update tabuada.js

--- web-development-002/exercise-006/tabuada.js
+++ web-development-002/exercise-006/tabuada.js
@@ -1,7 +1,7 @@
-var tabuada = function(n1) {
+var table = function(n1) {
     for (i = 0; i <= 10; i++) {
         console.log(n1 + ' x ' + i + " = " + n1 * i)
     }
 }
 
-module.exports = tabuada
+module.exports = table
