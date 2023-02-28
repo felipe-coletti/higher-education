6dfaed145dea043d8fbf8c9476b7e97657dbe8e5
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-02-28 10:07:15 -0300

    Create exercise-003.java

---
+++ programming-technique/exercise-003.java
@@ -0,0 +1,21 @@
+package javaapplication1;
+
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+
+public class JavaApplication1 {
+    public static void main(String[] args) {
+        String texto_1,  texto_2;
+        int valor_1, valor_2;
+        float moeda_1, moeda_2;
+        
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Digite um texto: ");
+        texto_1 = entrada.next();
+        System.out.println("Digite um número inteiro: ");
+        valor_1 = entrada.nextInt();
+        System.out.println("Digite um vlor monetário: ");
+        moeda_1 = entrada.nextFloat();
+                
+    }
+}
