1a2485120b1425e1a9a0b45805b516a4733d6638
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-02-28 09:46:49 -0300

    Create exercise-002.java

---
+++ programming-technique/exercise-002.java
@@ -0,0 +1,9 @@
+package javaapplication1;
+
+import javax.swing.JOptionPane;
+
+public class JavaApplication1 {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Olá mundo!");
+    }
+}
