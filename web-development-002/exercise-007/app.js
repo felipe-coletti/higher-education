037fc01fc7b4bcb3a29b74d04f60793b9879ebd6
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-03-01 10:53:32 -0300

    Update app.js

--- web-development-002/exercise-007/app.js
+++ web-development-002/exercise-007/app.js
@@ -13,6 +13,15 @@ app.get("/produto", function(req, res){
     res.send("Estou na página de produto.")
 })
 
+app.get("/produto/:item", function(req, res){
+    // res.send(req.params)
+    res.send("Item: " + req.params.item)
+})
+
+app.get("/produto/:item/:quantidade", function(req, res){
+    res.send("Item: " + req.params.item + "<br>Quantidade: " + req.params.quantidade)
+})
+
 app.listen(8081, function(){
     console.log("Servidor Ativo!")
 })
