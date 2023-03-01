e75dd8ac2dd8bd75d5222caa2976c77aee5acb67
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-03-01 10:08:39 -0300

    Update app.js

--- web-development-002/exercise-007/app.js
+++ web-development-002/exercise-007/app.js
@@ -1,24 +1,18 @@
 const express = require("express")
 const app = express()
 
-//Criando uma rota para o diretório principal do domínio
 app.get("/", function(req, res){
     res.send("<h1>Estou na primeira página.</h1>")
 })
 
-//Teste com uma outra página
 app.get("/contato", function(req, res){
     res.send("Estou na página de contato.")
 })
 
-//Teste com mais uma página
 app.get("/produto", function(req, res){
     res.send("Estou na página de produto.")
 })
 
-//Criando o servidor web na porta 8081
 app.listen(8081, function(){
     console.log("Servidor Ativo!")
 })
-
-//Nada pode vir após o listen. pois ele encerra a leitura do browser
