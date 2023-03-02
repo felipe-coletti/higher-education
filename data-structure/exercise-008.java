a3f8ee9e84d2ce1a3aedca37b9ae49a5681f0750
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-03-02 10:57:41 -0300

    Create exercise-008.java

---
+++ data-structure/exercise-008.java
@@ -0,0 +1,23 @@
+package javaapplication1;
+
+import java.util.Scanner;
+
+public class JavaApplication1 {
+    public static void main(String[] args) {
+        Scanner header = new scanner(System.in);
+        double media = 0
+        
+        for (int i = 0; i <= notas.length; i++) {
+            double[i] notas = header.nextDouble();
+            media += notas[i];
+        }
+        
+        media /= notas.length;
+        
+        for (int i = 0; i <= notas.length; i++) {
+            if (notas[i] >= media) {
+                System.out.println(notas[i]);
+            }
+        }
+    }
+}
