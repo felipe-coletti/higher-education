a9e373b3d310b650dfe2abfd830848adb257406b
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-03-02 09:22:41 -0300

    Create exercise-006.java

---
+++ data-structure/exercise-006.java
@@ -0,0 +1,14 @@
+package javaapplication1;
+
+import java.util.Scanner;
+
+public class JavaApplication1 {
+    public static void main(String[] args) {
+        String[] salaries = {};
+        
+        for (int i = 0; i <= salaries.length; i++) {
+            salaries[i] += salaries;
+            System.out.println(salaries);
+        }
+    }
+}
