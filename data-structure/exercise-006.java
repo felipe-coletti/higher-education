99d24c2319941d0f94baf78f87f2ac10bc9f4028
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-03-02 13:24:58 -0300

    Update exercise-006.java

--- data-structure/exercise-006.java
+++ data-structure/exercise-006.java
@@ -2,7 +2,7 @@ package javaapplication1;
 
 public class JavaApplication1 {
     public static void main(String[] args) {
-        String[] salaries = {};
+        String[] salaries = {1200, 5000, 7400};
         
         for (int i = 0; i <= salaries.length; i++) {
             salaries[i] += salaries;
