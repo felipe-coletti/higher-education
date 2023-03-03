9f59607855b9ee7651bb0ec06908b1d438f11c64
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-03-03 11:20:38 -0300

    Create exercise-009.java

---
+++ algorithms-and-programming-logic/exercise-009.java
@@ -0,0 +1,18 @@
+package javaapplication1;
+
+import java.util.Scanner;
+
+public class JavaApplication1 {
+    public static void main(String[] args) {
+        Scanner header = new Scanner(System.in);
+        
+        System.out.println("Digite a primeira nota: ");
+        double firstGrade = header.nextInt();
+        System.out.println("Digite a segunda nota: ");
+        double secondGrade = header.nextInt();
+        
+        double weightedAverage = (firstGrade * 2.5 + secondGrade * 4.5) / 2; 
+        
+        System.out.println("A média ponderada das notas é: " + weightedAverage + ".");
+    }
+}
