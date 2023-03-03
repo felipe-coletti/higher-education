1662923629a3afec460838562968c0f33fe7eb70
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-03-03 10:56:29 -0300

    Create exercise-007.java

---
+++ algorithms-and-programming-logic/exercise-007.java
@@ -0,0 +1,12 @@
+package javaapplication1;
+
+public class JavaApplication1 {
+    public static void main(String[] args) {
+        double a = 91.37;
+        double b = 7.351;
+        
+        double c = a + b;
+        
+        System.out.println("A soma de " + a + " e " + b + " é: " + c + ".");
+    }
+}
