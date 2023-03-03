1ccc200dfb0a08d544ae428b27d60cfea9e44c23
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-03-03 10:31:46 -0300

    Create exercise-005.java

---
+++ algorithms-and-programming-logic/exercise-005.java
@@ -0,0 +1,20 @@
+package javaapplication1;
+
+import java.util.Scanner;
+
+public class JavaApplication1 {
+    public static void main(String[] args) {
+        Scanner header = new Scanner(System.in);
+        
+        System.out.println("Digite o tempo gasto na viagem: ");
+        float t = header.nextFloat();
+        System.out.println("Digite a velocidade média do veículo durante a viagem: ");
+        float v = header.nextFloat();
+        
+        float d = t * v;
+        
+        float l = d / 12;
+        
+        System.out.println("A distância percorrida pelo veículo é de " + d + " Km e a quantidade de combustível gasta na viagem foi de " + l + " L.");
+    }
+}
