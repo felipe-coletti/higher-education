6452250a166d181c0d8ff61325143a5b3eda5c15
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-03-03 09:43:02 -0300

    Create exercise-004.java

---
+++ algorithms-and-programming-logic/exercise-004.java
@@ -0,0 +1,18 @@
+package javaapplication1;
+
+import java.util.Scanner;
+
+public class JavaApplication1 {
+    public static void main(String[] args) {
+        Scanner header = new Scanner(System.in);
+        
+        System.out.println("Digite o valor da base do retângulo: ");
+        float b = header.nextFloat();
+        System.out.println("Digite o valor da altura do retângulo: ");
+        float h = header.nextFloat();
+        
+        float a = b * h;
+        
+        System.out.println("A área do retângulo é: " + a);
+    }
+}
