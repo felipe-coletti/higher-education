f22b385d9f29318f5925c86a8ee5f5c36b8c8982
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-03-04 09:21:44 -0300

    Update exercise-05.java

--- algorithms-and-programming-logic/exercise-05.java
+++ algorithms-and-programming-logic/exercise-05.java
@@ -6,16 +6,16 @@ public class JavaApplication1 {
         int b[] = {3, 5, 6, 8, 10};
         int c[] = new int[5];
         
-        for(int i = 0; i < a.length; i++) {
+        for (int i = 0; i < a.length; i++) {
             System.out.print("O " + (i + 1) + "º número do vetor A é: " + a[i] + ".");
         }
         
-        for(int i = 0; i < b.length; i++) {
+        for (int i = 0; i < b.length; i++) {
             System.out.print("O " + (i + 1) + "º número do vetor B é: " + b[i] + ".");
             c[i] = (a[i] * b[i]);
         }
         
-        for(int i = 0; i < c.length; i++) {
+        for (int i = 0; i < c.length; i++) {
             System.out.print("O " + (i + 1) + "º número do vetor C é: " + c[i] + ".");
         }
     } 
