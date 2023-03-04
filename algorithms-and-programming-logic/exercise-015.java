7ef020c30f47a4ace804e5af5273db79770cd6a0
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-03-04 09:26:12 -0300

    Create exercise-015.java

---
+++ algorithms-and-programming-logic/exercise-015.java
@@ -0,0 +1,29 @@
+
+package javaapplication1;
+
+import java.util.Scanner;
+
+public class JavaApplication1 {
+    public static void main(String[] args) {
+        Scanner header = new Scanner(System.in);
+        
+        float grades[] = new float[3];
+        float average = 0;
+        
+        System.out.println("Digite as 3 notas: ");
+        
+        for(int i = 0; i < grades.length; i++) {
+            grades[i] = header.nextFloat();
+            average += grades[i];
+        }
+        
+        average /= grades.length;
+        
+        if (average >= 7) {
+            System.out.println("O aluno foi aprovado com a média: " + average + ".");
+        } else {
+            System.out.println("O aluno foi reprovado com a média: " + average + ".");
+        }
+        
+    }
+}
