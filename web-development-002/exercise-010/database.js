81f648bd8ef496ee64535c433589f2d7781eacb9
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-03-08 12:14:27 -0300

    Update database.js

--- web-development-002/exercise-010/database.js
+++ web-development-002/exercise-010/database.js
@@ -14,27 +14,21 @@ const schedules sequelize.define("schedules", {
     name: {
         type: sequelize.STRING
     },
-
     address: {
         type: sequelize.STRING
     },
-
     neighborhood: {
         type: sequelize.STRING
     },
-
     zipCode: {
         type: sequelize.INTEGER
     },
-
     city: {
         type: sequelize.STRING
     },
-
     state: {
         type: sequelize.STRING
     },
-
     note: {
         type: sequelize.STRING
     }
