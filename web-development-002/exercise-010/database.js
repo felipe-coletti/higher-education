02c90f729bf3691f4ba1708a271fb9ce46434948
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-03-08 11:31:43 -0300

    Update database.js

--- web-development-002/exercise-010/database.js
+++ web-development-002/exercise-010/database.js
@@ -1,5 +1,5 @@
 const sequelize = require("sequelize")
-const database = sequelize("example", "root", "", {
+const database = sequelize("test", "root", "", {
     host: "localhost",
     dialect: "mysql"
 })
