23671070da76b497604420500063ef11ac5e6f7a
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-03-09 08:52:36 -0300

    Create List.java

---
+++ data-structure/exercise-009/List.java
@@ -0,0 +1,13 @@
+package javaapplication1;
+
+public class List {
+    Knot start;
+    Knot end;
+    int length;
+    
+    List() {
+        start = null;
+        end = null;
+        length = 0;
+    }
+}
