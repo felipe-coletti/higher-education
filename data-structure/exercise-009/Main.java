7841fe542f60d8335dc2699ff61dbb9c9a12536c
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-03-09 10:55:57 -0300

    Create Main.java

---
+++ data-structure/exercise-009/Main.java
@@ -0,0 +1,12 @@
+package javaapplication1;
+
+public class Main {
+    public static void main(String[] args) {
+        List list = new List();
+        list.add("Ricardo");
+        list.add("Ana");
+        list.add("Aline");
+        list.printList();
+        list.printInfo(0);
+    } 
+}
