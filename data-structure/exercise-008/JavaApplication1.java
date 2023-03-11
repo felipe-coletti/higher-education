e2f4ae24e64a37394e1ab413f07d0169ebd9ad7f
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-03-11 10:05:38 -0300

    Update JavaApplication1.java

--- data-structure/exercise-008/JavaApplication1.java
+++ data-structure/exercise-008/JavaApplication1.java
@@ -4,20 +4,23 @@ import java.util.Scanner;
 
 public class JavaApplication1 {
     public static void main(String[] args) {
-        Scanner header = new scanner(System.in);
+        Scanner header = new Scanner(System.in);
         
-        double average = 0;
+        float grades[] = new float[5];
+        float average = 0;
         
-        for (int i = 0; i <= grades.length; i++) {
-            double[i] grades = header.nextDouble();
+        System.out.println("Digite as notas: ");
+        
+        for (int i = 0; i < grades.length; i++) {
+            grades[i] = header.nextFloat();
             
             average += grades[i];
         }
         
         average /= grades.length;
         
-        for (int i = 0; i <= grades.length; i++) {
-            if (grades[i] >= average) {
+        for (int i = 0; i < grades.length; i++) {
+            if (grades[i] > average) {
                 System.out.println(grades[i]);
             }
         }
