766da6cfae10ed8246d0faf4c5dd1076a3be9e95
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-03-11 09:11:38 -0300

    Create JavaApplication1.java

---
+++ algorithms-and-programming-logic/exercise-016/with-do-while/JavaApplication1.java
@@ -0,0 +1,26 @@
+package javaapplication1;
+
+import java.util.Scanner;
+
+public class JavaApplication1 {
+    public static void main(String[] args) {
+        Scanner header = new Scanner(System.in);
+        
+        int i = 0;
+        
+        do {
+            System.out.println("Digite a primeira nota: ");
+            double firstGrade = header.nextDouble();
+            System.out.println("Digite a segunda nota: ");
+            double secondGrade = header.nextDouble();
+            System.out.println("Digite a terceira nota: ");
+            double thirdGrade = header.nextDouble();
+            
+            double average = (firstGrade + secondGrade + thirdGrade) / 3;
+            
+            System.out.println("A média das notas do " + (i + 1) + "º aluno é: " + average + ".");
+            
+            i++;
+        } while (i <= 2);
+    }
+}
