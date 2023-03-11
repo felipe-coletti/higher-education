68616311b014ec3338caed27630aa8bee2622d00
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-03-11 09:12:41 -0300

    Update JavaApplication1.java

--- data-structure/exercise-003/JavaApplication1.java
+++ data-structure/exercise-003/JavaApplication1.java
@@ -11,7 +11,9 @@ public class JavaApplication1 {
         do {
             System.out.println("Digite um número inteiro: ");
             int number = header.nextInt();
+            
             result += number;
+            
             System.out.println("A soma é: " + result + ".");
         } while(number != 0);
     }
