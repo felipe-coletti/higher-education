8136b4882a0f2fb9cdfac220cf6ba883d65d5136
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-03-14 09:57:36 -0300

    Update and rename JavaApplication1.java to Main.java

--- programming-technique/exercise-005/Main.java
+++ programming-technique/exercise-005/Main.java
@@ -2,7 +2,7 @@ package javaapplication1;
 
 import javax.swing.JOptionPane;
 
-public class JavaApplication1 {
+public class Main {
     public static void main(String[] args) {
         Account account;
         
