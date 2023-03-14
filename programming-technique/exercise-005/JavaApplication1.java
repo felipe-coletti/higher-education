5c5a9c9fa1d204462b2070699d998b992a75a863
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-03-14 09:55:53 -0300

    Create JavaApplication1.java

---
+++ programming-technique/exercise-005/JavaApplication1.java
@@ -0,0 +1,26 @@
+package javaapplication1;
+
+import javax.swing.JOptionPane;
+
+public class JavaApplication1 {
+    public static void main(String[] args) {
+        Account account;
+        
+        String branch, number;
+        float balance, fee, depositValue, income;
+        
+        branch = JOptionPane.showInputDialog("Digite o número da agência: ");
+        number = JOptionPane.showInputDialog("Digite o número da conta: ");
+        balance = Float.parseFloat(JOptionPane.showInputDialog("Digte o saldo da conta: "));
+        fee = Float.parseFloat(JOptionPane.showInputDialog("Digite o valor da taxa: "));
+        
+        account = new Account(branch, number, balance, fee);
+        
+        account.printData();
+        income = account.calculateIncome();
+        JOptionPane.showMessageDialog(null, "O rendimento da conta é de R$ " + income + ".");
+        depositValue = Float.parseFloat(JOptionPane.showInputDialog("Digite o valor a ser depositado: "));
+        account.depositMoney(depositValue);
+        account.printData();
+    }
+}
