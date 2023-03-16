0fdeef2dc9f3a66181349f05c93312714c7fd0bf
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-03-16 08:59:22 -0300

    Update JavaApplication1.java

--- data-structure/exercise-010/JavaApplication1.java
+++ data-structure/exercise-010/JavaApplication1.java
@@ -1,7 +1,10 @@
 package javaapplication1;
 
+import java.util.Scanner;
+
 public class JavaApplication1 {
     public static void main(String[] args) {
+        /*
         int a = 4;
         int b = 2;
         int c = 0;
@@ -9,6 +12,25 @@ public class JavaApplication1 {
         for (int i = 0; i < b; i++) {
             c += a;
         }
+        
         System.out.println(c);
+        */
+        
+        Scanner header = new Scanner(System.in);
+        
+        float a = header.nextFloat();
+        float b = header.nextFloat();
+        
+        float c = multiplication(a, b);
+        
+        System.out.println(c);
+    }
+    
+    static float multiplication(float a, float b) {
+        for (int i = 0; i < b; i++) {
+            float c += a;
+        }
+        
+        return c;
     }
 }
