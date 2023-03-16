67cb5d2bde419bed670b53aa8d35685f73a902cb
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-03-16 08:39:15 -0300

    Create JavaApplication1.java

---
+++ data-structure/exercise-010/JavaApplication1.java
@@ -0,0 +1,14 @@
+package javaapplication1;
+
+public class JavaApplication1 {
+    public static void main(String[] args) {
+        int a = 4;
+        int b = 2;
+        int c = 0;
+        
+        for (int i = 0; i < b; i++) {
+            c += a;
+        }
+        System.out.println(c);
+    }
+}
