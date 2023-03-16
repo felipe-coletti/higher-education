3982ad7646e55bfb119d79d6b7951ee4ee9bc328
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-03-16 10:54:07 -0300

    Create Knot.java

---
+++ data-structure/exercise-012/Knot.java
@@ -0,0 +1,11 @@
+package javaapplication1;
+
+public class Knot {
+    Object info;
+    Knot pointer;
+    
+    public Knot(Object info) {
+        this.info = info;
+        pointer = null;
+    }
+}
