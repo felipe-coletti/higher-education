481786b3d4524ae2722ce51d25f0e300d16c7991
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-03-21 10:36:18 -0300

    Create Bulletin.java

---
+++ programming-technique/exercise-007/Bulletin.java
@@ -0,0 +1,31 @@
+package javaapplication1;
+
+import javax.swing.JOptionPane;
+
+public class Bulletin {
+    double firstGrade, secondGrade, average;
+    
+    public Bulletin(double firstGrade, double secondGrade) {
+        this.firstGrade = firstGrade;
+        this.secondGrade = secondGrade;
+        this.average = (firstGrade + secondGrade) / 2;
+    }
+    
+    String verifyConcept() {        
+        if (average >= 9) {
+            return "A";
+        } else if (average >= 7) {
+            return "B";
+        } else if (average >= 5) {
+            return "C";
+        } else if (average >= 4.5) {
+            return "D";
+        } else {
+            return "F";
+        }
+    }
+    
+    void printBullentin() {
+        JOptionPane.showMessageDialog(null, "Dados do boletim:\nNota 1: " + firstGrade + "\nNota 2: " + secondGrade + "\nMédia: " + average + "\nConceito: " + verifyConcept());
+    }
+}
