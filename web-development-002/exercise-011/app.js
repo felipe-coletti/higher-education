e8b157d4846d8fc15535c14996355cb3a7ced529
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-03-22 09:10:43 -0300

    Create app.js

---
+++ web-development-002/exercise-011/app.js
@@ -0,0 +1,14 @@
+const express = require("express")
+const app = express()
+const handlebars= require("express-handlebars").engine
+
+app.engine("handlebars", handlebars({defaultLayout: "main"}))
+app.set("view engine", "handlebars")
+
+app.get("/", function(req, res){
+    res.render("first-page")
+})
+
+app.listen(8081, function(){
+    console.log("Servidor Ativo!")
+})
