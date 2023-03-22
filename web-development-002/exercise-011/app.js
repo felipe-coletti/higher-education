1f98d1187ddb5eec0e3533bc54db759fb13f0c7d
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-03-22 11:10:26 -0300

    Update app.js

--- web-development-002/exercise-011/app.js
+++ web-development-002/exercise-011/app.js
@@ -9,6 +9,10 @@ app.get("/", function(req, res){
     res.render("first-page")
 })
 
+app.post("/sign-up", function(req, res){
+    res.send("Formulário recebido.")
+})
+
 app.listen(8081, function(){
     console.log("Servidor Ativo!")
 })
