7c210bbd87e0253897fdaf78afcc01a2a56f0705
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-03-22 08:55:34 -0300

    Update and rename web-development-002/exercise-003/app.js to web-development-002/exercise-002/app.js

--- web-development-002/exercise-002/app.js
+++ web-development-002/exercise-002/app.js
@@ -2,18 +2,14 @@ var a = 10
 var b = 11
 var total = 0
 
-// adição
 total = a + b
 console.log(total)
 
-// subtração
 total = a - b
 console.log(total)
 
-// multiplicação
 total = a * b
 console.log(total)
 
-// divisão
 total = a / b
 console.log(total)
