7b8c4a56bb07e38bc38139df0ffc24b1609aeca7
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-03-23 09:36:06 -0300

    Create Main.java

---
+++ programming-technique/exercise-009/Main.java
@@ -0,0 +1,11 @@
+package javaapplication1;
+
+import javax.swing.JOptionPane;
+
+public class Main {
+    public static void main(String[] args) {
+        String consumption = JOptionPane.showInputDialog("Digite o consumo de energia elétrica em kilowatts hora: ");
+        
+        JOptionPane.showMessageDialog(null, bill(consumption));
+    }
+}
