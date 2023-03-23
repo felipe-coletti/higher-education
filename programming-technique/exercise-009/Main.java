fcad7beb3874351ab45e53e4ba45bcb3e5d4dbb1
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-03-23 18:30:36 -0300

    Update Main.java

--- programming-technique/exercise-009/Main.java
+++ programming-technique/exercise-009/Main.java
@@ -4,8 +4,10 @@ import javax.swing.JOptionPane;
 
 public class Main {
     public static void main(String[] args) {
+        Cost cost = new Cost(consumption); 
+        
         String consumption = JOptionPane.showInputDialog("Digite o consumo de energia elétrica em kilowatts hora: ");
         
-        printBill(consumption);
+        printBill();
     }
 }
