3b80f73d52d00b775a98c6969187347d2f4c5c4b
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-03-23 09:31:30 -0300

    Update Cost.java

--- programming-technique/exercise-009/Cost.java
+++ programming-technique/exercise-009/Cost.java
@@ -11,50 +11,50 @@ public class Cost {
     }
   
     float ICMS() {
-            if (consumption <= 200) {
-                aliquot = 0.12;
-                multiplicationFactor = 0.136363;
-            } else {
-                aliquot = 0.25;
-                multiplicationFactor = 0.333333;
-            }
-          
-            return multiplicationFactor * suply();
+        if (consumption <= 200) {
+            aliquot = 0.12;
+            multiplicationFactor = 0.136363;
+        } else {
+            aliquot = 0.25;
+            multiplicationFactor = 0.333333;
         }
+          
+        return multiplicationFactor * suply();
+    }
       
-        float COFINS() {
-            aliquot = 0.05033;
+    float COFINS() {
+        aliquot = 0.05033;
           
-            if (consumption <= 200) {
-                multiplicationFactor = 0.0614722;
-            } else {
-                multiplicationFactor = 0.0730751;
-            }
-          
-            return multiplicationFactor * suply();
+        if (consumption <= 200) {
+            multiplicationFactor = 0.0614722;
+        } else {
+            multiplicationFactor = 0.0730751;
         }
+          
+        return multiplicationFactor * suply();
+    }
       
-        float PIS() {
-            aliquot = 0.010927;
+    float PIS() {
+        aliquot = 0.010927;
           
-            if (consumption <= 200) {
-                multiplicationFactor = 0.013346;
-            } else {
-                multiplicationFactor = 0.0158651;
-            }
+        if (consumption <= 200) {
+            multiplicationFactor = 0.013346;
+        } else {
+            multiplicationFactor = 0.0158651;
+        }
           
-            return multiplicationFactor * suply();
-        }
+        return multiplicationFactor * suply();
+    }
   
-        float icmsCofins() {
-            return COFINS() * ICMS() * suply;
-        }
+    float icmsCofins() {
+        return COFINS() * ICMS() * suply;
+    }
   
-        float icmsPis() {
-            return PIS() * ICMS() * suply;
-        }
+    float icmsPis() {
+        return PIS() * ICMS() * suply;
+    }
   
-        String bill() {
-            JOptionPane.showMessageDialog(null, "Dados da fatura:\nFornecimento: " + suply + "\nICMS: " + ICMS() + "\nCOFINS: " + COFINS() + "\nPIS/PASESP: " + PIS() + "\nICMS sobre COFINS: " + icmsCofins() + "\nICMS sobre PIS/PASESP: " + icmsPis());
-        }
+    String bill() {
+        JOptionPane.showMessageDialog(null, "Dados da fatura:\nFornecimento: " + suply + "\nICMS: " + ICMS() + "\nCOFINS: " + COFINS() + "\nPIS/PASESP: " + PIS() + "\nICMS sobre COFINS: " + icmsCofins() + "\nICMS sobre PIS/PASESP: " + icmsPis());
+    }
 }
