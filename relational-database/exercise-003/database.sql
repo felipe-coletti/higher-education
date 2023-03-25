a12e0dfc97e5b1c2c67ef20bb203c0b65ae2940b
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-03-25 11:34:13 -0300

    Update database.sql

--- relational-database/exercise-003/database.sql
+++ relational-database/exercise-003/database.sql
@@ -1,1 +1,59 @@
 
+create database parking;
+
+use parking;
+
+create table product (
+	id int auto_increment,
+	cost float(6, 2),
+	description varchar(255),
+	primary key(id)
+);
+
+create table service (
+	id int auto_increment,
+	cost float(6, 2),
+	time time,
+	description varchar(255),
+	primary key(id)
+);
+
+create table budget (
+	id int auto_increment,
+	value float(6, 2),
+	situation varchar(255),
+	date date,
+	entryForecast datetime,
+	paymentMethod varchar(255),
+	constraint vehicleId foreign key (id) references vehicle (id),
+	primary key(id)
+);
+
+create table client (
+	cpf varchar(14),
+	name varchar(255),
+	telephone varchar(19),
+	address varchar(255),
+	primary key(cpf)
+);
+
+create table vehicle (
+	renavam int(11),
+	chassis varchar(255),
+	model varchar(255),
+	color varchar(255),
+	constraint clientId foreign key (id) references client (id),
+	primary key(renavam)
+);
+
+create table clientReport (
+	id int auto_increment,
+	problem varchar(255),
+	date date,
+	constraint vehicleId foreign key (id) references vehicle (id),
+	primary key(id)
+);
+
+alter table client modify column name completName varchar(255);
+
+alter table vehicle add column year int(4);
