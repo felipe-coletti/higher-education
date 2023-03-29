89b70492da538ed45ae96727ef47256629e8b08b
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-03-29 13:26:11 -0300

    Update Main.java

--- programming-technique/exercise-005/Main.java
+++ programming-technique/exercise-005/Main.java
@@ -6,20 +6,21 @@ public class Main {
     public static void main(String[] args) {
         Account account;
         
-        String branch, number;
-        float balance, fee, depositValue, income;
-        
-        branch = JOptionPane.showInputDialog("Digite o número da agência: ");
-        number = JOptionPane.showInputDialog("Digite o número da conta: ");
-        balance = Float.parseFloat(JOptionPane.showInputDialog("Digite o saldo da conta: "));
-        fee = Float.parseFloat(JOptionPane.showInputDialog("Digite o valor da taxa: "));
+        String branch = JOptionPane.showInputDialog("Digite o número da agência: ");
+        String number = JOptionPane.showInputDialog("Digite o número da conta: ");
+        float balance = Float.parseFloat(JOptionPane.showInputDialog("Digite o saldo da conta: "));
+        float fee = Float.parseFloat(JOptionPane.showInputDialog("Digite o valor da taxa: "));
         
         account = new Account(branch, number, balance, fee);
         
         account.printData();
-        income = account.calculateIncome();
+        
+        float income = account.calculateIncome();
+        
         JOptionPane.showMessageDialog(null, "O rendimento da conta é de R$ " + income + ".");
-        depositValue = Float.parseFloat(JOptionPane.showInputDialog("Digite o valor a ser depositado: "));
+        
+        float depositValue = Float.parseFloat(JOptionPane.showInputDialog("Digite o valor a ser depositado: "));
+        
         account.depositMoney(depositValue);
         account.printData();
     }
