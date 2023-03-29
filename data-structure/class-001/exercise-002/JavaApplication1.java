945c8b47a4ba55e8af6d64b7398c2fc9a783627b
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-03-29 00:09:02 -0300

    Update JavaApplication1.java

--- data-structure/class-001/exercise-002/JavaApplication1.java
+++ data-structure/class-001/exercise-002/JavaApplication1.java
@@ -5,7 +5,7 @@ public class JavaApplication1 {
         int number = 5;
         int result = number;
         
-        for (int i = number - 1; i >= 1; i--) {
+        for (int i = number - 1; i > 1; i--) {
             result *= i;
             
             System.out.println(result / i + " x " + i + " = " + result);
