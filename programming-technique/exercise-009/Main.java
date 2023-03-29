1255c8e8bab2ee75310874098e7e210c06bfe257
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-03-29 13:15:20 -0300

    Update Main.java

--- programming-technique/exercise-009/Main.java
+++ programming-technique/exercise-009/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
         Cost cost; 
         
-        String consumption = JOptionPane.showInputDialog("Digite o consumo de energia elétrica em kilowatts hora: ");
+        double consumption = Double.parseDouble(JOptionPane.showInputDialog("Digite o consumo de energia elétrica em kilowatts hora: "));
         
         cost = new Cost(consumption); 
         
