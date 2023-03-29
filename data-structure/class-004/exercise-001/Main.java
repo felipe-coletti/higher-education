663054550ef5966273ad4edf727dc01c97fd5595
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-03-29 14:11:12 -0300

    Update Main.java

--- data-structure/class-004/exercise-001/Main.java
+++ data-structure/class-004/exercise-001/Main.java
@@ -3,9 +3,16 @@ package javaapplication1;
 public class Main {
     public static void main(String[] args) {
         List list = new List();
-        list.add("Ricardo", "123456789");
-        list.add("Ricardo", "987654321");
+        
+        list.add("Ricardo", "958455865");
+        list.add("Ana", "964582315");
+        list.add("Aline", "9354784898");
+        list.add("Ricardo", "956855485");
+        
         list.printList();
+        
+        list.findTelephone("Ana");
+        list.findTelephone("Maria");
         list.findTelephone("Ricardo");
     } 
 }
