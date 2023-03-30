1e94e04291603a6c42360bb95d684aa479875d0b
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-03-30 18:04:20 -0300

    Create Date.java

---
+++ programming-technique/class-002/exercise-001/Date.java
@@ -0,0 +1,9 @@
+public class Date {
+    int day, month, year;
+
+    public Date(int day, int month, int year) {
+        this.day = day;
+        this.month = month;
+        this.year = year;
+    }
+}
