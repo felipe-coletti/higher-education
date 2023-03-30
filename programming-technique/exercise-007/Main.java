d347bf05430757deab87ca1fcb181a1aa2b3482d
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-03-30 10:08:54 -0300

    Update Main.java

--- programming-technique/exercise-007/Main.java
+++ programming-technique/exercise-007/Main.java
@@ -2,7 +2,8 @@ package javaapplication1;
 
 public class Main {
     public static void main(String[] args) {
-        Bulletin bulletin = new Bulletin(5.6, 4.5);      
+        Bulletin bulletin = new Bulletin(5.6, 4.5);
+        
         bulletin.printData();
     }
 }
