601815f49ea0ff8e802e4b76a6b6301f76f1b616
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-03-30 11:17:20 -0300

    Update Participant.java

--- programming-technique/exercise-010/Participant.java
+++ programming-technique/exercise-010/Participant.java
@@ -10,6 +10,22 @@ public class Participant {
         this.name = name;
         this.age = age;
     }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public int getAge() {
+        return age;
+    }
+    
+    public void setAge(int age) {
+        this.age = age;
+    }
   
     String verifyCategory() {        
         if (age >= 30) {
