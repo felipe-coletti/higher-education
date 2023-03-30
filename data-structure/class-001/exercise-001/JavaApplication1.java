35ab94e6f75a421f8e2860bbed2b6a203ef9cc81
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-03-30 13:28:33 -0300

    Update JavaApplication1.java

--- data-structure/class-001/exercise-001/JavaApplication1.java
+++ data-structure/class-001/exercise-001/JavaApplication1.java
@@ -1,4 +1,3 @@
-/*
 package javaapplication1;
 
 public class JavaApplication1 {
@@ -7,21 +6,6 @@ public class JavaApplication1 {
             if (i % 2 == 0) {
                 System.out.println(i);
             }
-        }
-    }
-}
-*/
-
-package javaapplication1;
-
-public class JavaApplication1 {
-    public static void main(String[] args) {
-        for (int i = 1; i <= 10; i += 2) {
-            if (i % 2 != 0) {
-                i++;
-            }
-            
-            System.out.println(i);
         }
     }
 }
