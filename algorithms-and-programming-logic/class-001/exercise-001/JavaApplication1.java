ec18bca7718ec2af3caaf853d3396557731c452f
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-03-30 15:26:45 -0300

    Update JavaApplication1.java

--- algorithms-and-programming-logic/class-001/exercise-001/JavaApplication1.java
+++ algorithms-and-programming-logic/class-001/exercise-001/JavaApplication1.java
@@ -6,8 +6,6 @@ public class JavaApplication1 {
     public static void main(String[] args) {
         Scanner header = new Scanner(System.in);
         
-        // Entrada
-        
         System.out.println("Digite o código da peça: ");
         int pieceCode = header.nextInt();
         System.out.println("Digite o valor da peça: ");
@@ -15,11 +13,7 @@ public class JavaApplication1 {
         System.out.println("Digite a quantidade de peças: ");
         int pieceQuantity = header.nextInt();
         
-        // Processamento
-        
         float totalValue = pieceValue * pieceQuantity;
-        
-        // Saída
         
         System.out.println("O valor total das peças com o código " + pieceCode + " é: R$ " + totalValue + ".");
     }
