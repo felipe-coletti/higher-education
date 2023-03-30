fbefa7c34331bdc60f34db43f43499b8703e23e8
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-03-30 15:09:48 -0300

    Update Cost.java

--- programming-technique/exercise-009/Cost.java
+++ programming-technique/exercise-009/Cost.java
@@ -3,55 +3,34 @@ package javaapplication1;
 import javax.swing.JOptionPane;
 
 public class Cost {
-    double consumption, fare, aliquot, multiplicationFactor, suply;
-  
+    double consumption, fare, suply, icms, cofins, pis, icmsCofins, icmsPis;
+
     public Cost(double consumption) {
         this.consumption = consumption;
         this.fare =  0.28172;
         this.suply = consumption * fare;
+        this.icms = icms;
+        this.cofins = cofins;
+        this.pis = pis;
+        this.icmsCofins = icmsCofins;
+        this.icmsPis = icmsPis;
     }
-  
-    double icms() {
-        if (consumption <= 200) {
-            aliquot = 0.12;
-            multiplicationFactor = 0.136363;
-        } else {
-            aliquot = 0.25;
-            multiplicationFactor = 0.333333;
-        }
-          
-        return multiplicationFactor * suply * aliquot;
-    }
-      
-    double cofins() {
-        aliquot = 0.05033;
-          
-        if (consumption <= 200) {
-            multiplicationFactor = 0.0614722;
-        } else {
-            multiplicationFactor = 0.0730751;
-        }
-          
-        return multiplicationFactor * suply * aliquot;
-    }
-      
-    double pis() {
-        aliquot = 0.010927;
-          
-        if (consumption <= 200) {
-            multiplicationFactor = 0.013346;
-        } else {
-            multiplicationFactor = 0.0158651;
-        }
-          
-        return multiplicationFactor * suply * aliquot;
-    }
-  
-    double icmsCofins = cofins() * icms() * suply;
-  
-    double icmsPis = pis() * icms() * suply;
-  
+
     void printBill() {
-        JOptionPane.showMessageDialog(null, "Dados da fatura:\nFornecimento: " + suply + "\nICMS: " + icms() + "\nCOFINS: " + cofins() + "\nPIS/PASESP: " + pis() + "\nICMS sobre COFINS: " + icmsCofins + "\nICMS sobre PIS/PASESP: " + icmsPis);
+        if (consumption <= 200) {
+            this.icms = suply * 0.136363;
+            this.cofins = suply * 0.0614722;
+            this.pis = suply * 0.013346;
+            this.icmsCofins = suply * 0.136363 * 0.0614722;
+            this.icmsPis = suply * 0.136363 * 0.013346;
+        } else {
+            this.icms = suply * 0.333333;
+            this.cofins = suply * 0.0730751;
+            this.pis = suply * 0.0158651;
+            this.icmsCofins = suply * 0.333333 * 0.0730751;
+            this.icmsPis = suply * 0.333333 * 0.0158651;
+        }
+        
+        JOptionPane.showMessageDialog(null, "Dados da fatura:\nFornecimento: " + suply + "\nICMS: " + icms + "\nCOFINS: " + cofins + "\nPIS/PASESP: " + pis + "\nICMS sobre COFINS: " + icmsCofins + "\nICMS sobre PIS/PASESP: " + icmsPis);
     }
 }
