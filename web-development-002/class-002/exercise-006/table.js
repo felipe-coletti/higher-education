548694d9749a13d10196c7a0ec0d523ad5dce134
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-03-30 23:08:28 -0300

    Update and rename web-development-002/exercise-007/table.js to web-development-002/class-002/exercise-006/table.js

---
+++ web-development-002/class-002/exercise-006/table.js
@@ -0,0 +1,7 @@
+var table = function(a) {
+    for (i = 0; i <= 10; i++) {
+        console.log(a + ' x ' + i + " = " + a * i)
+    }
+}
+
+module.exports = table
--- web-development-002/exercise-007/table.js
+++
@@ -1,7 +0,0 @@
-var table = function(n1) {
-    for (i = 0; i <= 10; i++) {
-        console.log(n1 + ' x ' + i + " = " + n1 * i)
-    }
-}
-
-module.exports = table
