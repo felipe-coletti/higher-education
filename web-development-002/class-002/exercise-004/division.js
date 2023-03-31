2ae0053e02ecb6d50ef2019927cbdc06e0d036a7
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-03-30 23:03:48 -0300

    Update and rename web-development-002/exercise-005/division.js to web-development-002/class-002/exercise-004/division.js

---
+++ web-development-002/class-002/exercise-004/division.js
@@ -0,0 +1,5 @@
+var division = function(a, b) {
+    return a / b
+}
+
+module.exports = division
--- web-development-002/exercise-005/division.js
+++
@@ -1,5 +0,0 @@
-var division = function(n1, n2) {
-    return n1 / n2
-}
-
-module.exports = division
