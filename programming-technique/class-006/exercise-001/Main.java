478ddba16d1dd267dfb54ed12459951a255bcb0f
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-04-04 10:19:34 -0300

    Update Main.java

--- programming-technique/class-006/exercise-001/Main.java
+++ programming-technique/class-006/exercise-001/Main.java
@@ -4,18 +4,18 @@ import javax.swing.JOptionPane;
 
 public class Main {
     public static void main(String[] args) {
-        Debt debt = new Debt("João", "(00) 91234-5678", 125.40f);
+        Client client = new Client("João", "(00) 91234-5678", 125.40f);
         
-        debt.setName("");
+        client.setName("");
         
-        debt.printData();
+        client.printData();
         
-        JOptionPane.showMessageDialog(null, "Juros da dívida: " + debt.calculateInterest(1.5f));
+        JOptionPane.showMessageDialog(null, "Juros da dívida: " + client.calculateDebtInterest(1.5f));
         
-        Purchase purchase = new Purchase("João", "(00) 91234-5678", 1500.89f);
+        Supplier supplier = new Supplier("João", "(00) 91234-5678", 1500.89f);
         
-        purchase.printData();
+        supplier.printData();
         
-        JOptionPane.showMessageDialog(null, "Imposto da compra: " + purchase.calculateTax(12.5f));
+        JOptionPane.showMessageDialog(null, "Imposto da compra: " + supplier.calculatePurchaseTax(12.5f));
     }
 }
