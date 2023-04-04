5859530779ae15f2658908254de272c8ec095786
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-04-04 09:37:50 -0300

    Create Supplier.java

---
+++ programming-technique/class-006/exercise-001/Supplier.java
@@ -0,0 +1,29 @@
+package javaapplication1;
+
+import javax.swing.JOptionPane;
+
+public class Supplier extends Person {
+    private float purchasePrice;
+    
+    public Supplier(String name, String telephone, float purchasePrice) {
+        super(name, telephone);
+        this.purchasePrice = purchasePrice;
+    }
+    
+    public float getPurchasePrice() {
+        return purchasePrice;
+    }
+
+    public void setPurchasePrice(float purchasePrice) {
+        this.purchasePrice = purchasePrice;
+    }
+    
+    public void printData() {
+        super.printData();
+        JOptionPane.showMessageDialog(null, "Valor da compra: " + purchasePrice);
+    }
+    
+    public float calculatePurchasePrice(float interestRate) {
+        return purchasePrice += interestRate / 100 * purchasePrice;
+    }
+}
