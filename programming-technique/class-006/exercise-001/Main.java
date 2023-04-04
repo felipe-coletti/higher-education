357d33b03d11bad3e480fc67c9fd0b96d24613ae
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-04-04 10:27:05 -0300

    Update Main.java

--- programming-technique/class-006/exercise-001/Main.java
+++ programming-technique/class-006/exercise-001/Main.java
@@ -4,15 +4,15 @@ import javax.swing.JOptionPane;
 
 public class Main {
     public static void main(String[] args) {
-        Client client = new Client("João", "(00) 91234-5678", 125.40f);
+        Client client = new Client("João", "(99) 99999-9999", 125.40f);
         
-        client.setName("");
+        client.setName("Maria");
         
         client.printData();
         
         JOptionPane.showMessageDialog(null, "Juros da dívida: " + client.calculateDebtInterest(1.5f));
         
-        Supplier supplier = new Supplier("João", "(00) 91234-5678", 1500.89f);
+        Supplier supplier = new Supplier("João", "(99) 99999-9999", 1500.89f);
         
         supplier.printData();
         
