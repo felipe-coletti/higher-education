877fa691da18f3293e12eb12c3b127c2ef873fce
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-04-05 09:33:02 -0300

    Update app.js

--- web-development-002/class-005/exercise-001/app.js
+++ web-development-002/class-005/exercise-001/app.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const app = express()
-const handlebars= require("express-handlebars").engine
+const handlebars = require("express-handlebars").engine
+const bodyParser = require("body-parser")
 
 app.engine("handlebars", handlebars({defaultLayout: "main"}))
 app.set("view engine", "handlebars")
