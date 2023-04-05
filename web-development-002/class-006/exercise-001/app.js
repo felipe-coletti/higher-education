5c1cc9b146448ea7979de6d1b5dde7ca96a1390e
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-04-05 11:04:18 -0300

    Update app.js

--- web-development-002/class-006/exercise-001/app.js
+++ web-development-002/class-006/exercise-001/app.js
@@ -28,6 +28,14 @@ app.post("/sign-up", function(req, res){
     })
 })
 
+app.get("/consult", function(req, res){
+    post.findAll().then(function(post){
+        res.render("consult", {post})
+    }).catch(function(error){
+        console.log("Erro ao carregar os dados: " + error)
+    })
+})
+
 app.listen(8081, function(){
     console.log("Servidor Ativo!")
 })
