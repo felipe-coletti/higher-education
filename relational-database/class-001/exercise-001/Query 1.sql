03eb3b63bfbdf9f13c95b029c255af50a423ac9e
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-04-06 14:54:14 -0300

    Update Query 1.sql

--- relational-database/class-001/exercise-001/Query 1.sql
+++ relational-database/class-001/exercise-001/Query 1.sql
@@ -15,14 +15,14 @@ create table discipline (
 	id int not null auto_increment,
 	name varchar(255) not null,
 	module int(1) not null,
-	constraint studentRa foreign key (ra) references student (ra),
-	primary key (id)
+	primary key (id),
+	constraint studentRa foreign key (ra) references student (ra)
 );
 
 create table grade (
 	id int not null auto_increment,
 	value decimal(2, 2) not null check (value between 0 and 10),
 	constraint studentRa foreign key (ra) references student(ra),
-	constraint disciplineId foreign key (id) references discipline (id),
-	primary key (id)
+	primary key (id),
+	constraint disciplineId foreign key (id) references discipline (id)
 );
