9b6515916a665d87de5d12baa8cead3f6ce3249c
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-04-06 13:34:53 -0300

    Update Main.java

--- data-structure/class-004/exercise-001/Main.java
+++ data-structure/class-004/exercise-001/Main.java
@@ -4,10 +4,10 @@ public class Main {
     public static void main(String[] args) {
         List list = new List();
         
-        list.add("Ricardo", "958455865");
-        list.add("Ana", "964582315");
-        list.add("Aline", "9354784898");
-        list.add("Ricardo", "956855485");
+        list.addItem("Ricardo", "958455865");
+        list.addItem("Ana", "964582315");
+        list.addItem("Aline", "9354784898");
+        list.addItem("Ricardo", "956855485");
         
         list.printList();
         
