fb4a14c3b80010dca86d99d7422ea7ca63650d4b
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-04-06 10:30:44 -0300

    Create Main.java

---
+++ data-structure/class-005/Main.java
@@ -0,0 +1,35 @@
+package javaapplication1;
+
+import javax.swing.JOptionPane;
+
+public class Main {
+    public static void main(String[] args) {
+        Queue queue = new Queue();
+        
+        int option;
+        String task;
+        
+        do {
+            option = Integer.parseInt(JOptionPane.showInputDialog("Digite:\n1 para enfileirar;\n2 para início;\n3 para desenfileirar;\nOu 0 para voltar."));
+            
+            switch (option) {
+                case 1:
+                    task = JOptionPane.showInputDialog("Digite uma tarefa: ");
+                    queue.toQueue(task);
+                    break;
+                case 2:
+                    task = (String) queue.start();
+                    JOptionPane.showMessageDialog(null, task);
+                    break;
+                case 3:
+                    task = (String) queue.unqueue();
+                    JOptionPane.showMessageDialog(null, task);
+                    break;
+                case 0:
+                    break;
+                default:
+                    System.out.println("Opção inválida.");
+            }
+        } while (option != 0);
+    }
+}
