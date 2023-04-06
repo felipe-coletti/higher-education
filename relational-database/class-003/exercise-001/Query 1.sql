4e6f755c63e4a611cabb45535097038de483b5b3
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-04-06 15:28:13 -0300

    Update Query 1.sql

--- relational-database/class-003/exercise-001/Query 1.sql
+++ relational-database/class-003/exercise-001/Query 1.sql
@@ -68,7 +68,7 @@ create table serviceItem (
 	situation varchar(255)
 );
 
-alter table client modify column name completName varchar(255);
+alter table client change column name completeName varchar(255);
 
 alter table vehicle add column year int(4);
 
