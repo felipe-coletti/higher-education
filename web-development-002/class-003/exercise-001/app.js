18973a3f55b9138b3de0cf2877858b94ad058c91
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-04-12 12:00:00 -0300

    Update app.js

--- web-development-002/class-003/exercise-001/app.js
+++ web-development-002/class-003/exercise-001/app.js
@@ -1,26 +1,26 @@
 const express = require("express")
 const app = express()
 
-app.get("/", function(req, res){
+app.get("/", function(req, res) {
     res.sendFile(__dirname + "/html/index.html")
 })
 
-app.get("/contact", function(req, res){
+app.get("/contact", function(req, res) {
     res.send("Estou na página de contato.")
 })
 
-app.get("/product", function(req, res){
+app.get("/product", function(req, res) {
     res.send("Estou na página de produto.")
 })
 
-app.get("/product/:item", function(req, res){
+app.get("/product/:item", function(req, res) {
     res.send("Item: " + req.params.item)
 })
 
-app.get("/product/:item/:quantity", function(req, res){
+app.get("/product/:item/:quantity", function(req, res) {
     res.send("Item: " + req.params.item + " Quantidade: " + req.params.quantity)
 })
 
-app.listen(8081, function(){
+app.listen(8081, function() {
     console.log("Servidor Ativo!")
 })
