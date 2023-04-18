e7800e9408eb76a16f8e13b3f713b8ea18905e00
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-04-18 10:59:57 -0300

    Update JavaApplication1.java

--- programming-technique/class-007/exercise-002/JavaApplication1.java
+++ programming-technique/class-007/exercise-002/JavaApplication1.java
@@ -6,16 +6,16 @@ public class JavaApplication1 {
     public static void main(String[] args) {
         String[] names = new String[4];
         Float[] grades = new Float[4];
-        float sum = 0;
+        float average = 0;
         
         for (int i = 0; i < 4; i++) {
             names[i] = JOptionPane.showInputDialog("Digite o nome do " + (i + 1) + "º aluno.");
             grades[i] = Float.parseFloat(JOptionPane.showInputDialog("Digite a nota do " + (i + 1) + "º aluno."));
             
-            sum += grades[i];
+            average += grades[i];
         }
         
-        float average = sum / 4;
+        average /= grades.length;
         
         for (int i = 0; i < 4; i++) {
             if (grades[i] > average) {
