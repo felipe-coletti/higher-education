3d24c604bc3c8f83c9057d2b4a8c70f719cdb814
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-04-19 08:59:36 -0300

    Update JavaApplication1.java

--- data-structure/class-003/exercise-001/JavaApplication1.java
+++ data-structure/class-003/exercise-001/JavaApplication1.java
@@ -2,12 +2,12 @@ package javaapplication1;
 
 public class JavaApplication1 {
     public static void main(String[] args) {
-        String[] salaries = {1200, 5000, 7400};
+        int[] salaries = {1200, 5000, 7400};
         
-        for (int i = 0; i <= salaries.length; i++) {
+        for (int i = 0; i < salaries.length; i++) {
             salaries[i] += 200;
             
-            System.out.println(salaries);
+            System.out.println(salaries[i]);
         }
     }
 }
