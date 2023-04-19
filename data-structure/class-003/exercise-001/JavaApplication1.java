61e22cb175d51adda9bed8d45854ce57537285e7
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-04-19 09:03:15 -0300

    Update JavaApplication1.java

--- data-structure/class-003/exercise-001/JavaApplication1.java
+++ data-structure/class-003/exercise-001/JavaApplication1.java
@@ -2,7 +2,7 @@ package javaapplication1;
 
 public class JavaApplication1 {
     public static void main(String[] args) {
-        int[] salaries = {1200, 5000, 7400};
+        float[] salaries = {1200, 5000, 7400};
         
         for (int i = 0; i < salaries.length; i++) {
             salaries[i] += 200;
