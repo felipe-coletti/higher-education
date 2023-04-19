b9f58087fc11203a957f786088273d37f5dcb1e3
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-04-18 21:51:26 -0300

    Update JavaApplication1.java

--- data-structure/class-001/exercise-002/JavaApplication1.java
+++ data-structure/class-001/exercise-002/JavaApplication1.java
@@ -4,11 +4,13 @@ public class JavaApplication1 {
     public static void main(String[] args) {
         int number = 5;
         int result = number;
+        String count = String.valueOf(number);
         
-        for (int i = number - 1; i > 1; i--) {
+        for (int i = number - 1; i > 0; i--) {
+            count += " x " + i;
             result *= i;
-            
-            System.out.println(result / i + " x " + i + " = " + result);
         }
+        
+        System.out.println(number + "! = " + count + " = " + result);
     }
 }
