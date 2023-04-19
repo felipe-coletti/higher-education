ee7f135cd8ef19d8c418ff5af73967cc39354bd1
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-04-19 12:23:44 -0300

    Update JavaApplication1.java

--- programming-technique/class-001/exercise-002/JavaApplication1.java
+++ programming-technique/class-001/exercise-002/JavaApplication1.java
@@ -4,6 +4,6 @@ import javax.swing.JOptionPane;
 
 public class JavaApplication1 {
     public static void main(String[] args) {
-        JOptionPane.showMessageDialog(null, "Olá mundo!");
+        JOptionPane.showMessageDialog(null, "Olá, mundo!");
     }
 }
