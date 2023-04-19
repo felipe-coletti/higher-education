3d252202ce39c7f8fe7bb8b4898b2cb055c7dd99
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-04-19 00:11:26 -0300

    Update JavaApplication1.java

--- algorithms-and-programming-logic/class-007/exercise-001/JavaApplication1.java
+++ algorithms-and-programming-logic/class-007/exercise-001/JavaApplication1.java
@@ -8,7 +8,7 @@ public class JavaApplication1 {
         
         float grades[] = new float[9];
         
-        System.out.println("Digite as 10 notas: ");
+        System.out.println("Digite 10 notas: ");
         
         for (int i = 0; i < grades.length; i++) {
             grades[i] = header.nextFloat();
