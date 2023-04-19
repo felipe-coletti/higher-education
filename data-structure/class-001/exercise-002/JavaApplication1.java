2d4b8f53893c0d0cc79fa9fd3f9a92736ca496b8
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-04-18 21:48:07 -0300

    Update JavaApplication1.java

--- data-structure/class-001/exercise-002/JavaApplication1.java
+++ data-structure/class-001/exercise-002/JavaApplication1.java
@@ -2,7 +2,8 @@ package javaapplication1;
 
 public class JavaApplication1 {
     public static void main(String[] args) {
-        int number, result = 5;
+        int number = 5;
+        int result = number;
         
         for (int i = number - 1; i > 1; i--) {
             result *= i;
