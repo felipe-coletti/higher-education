3957f37345eeb049a3f3a8ed342f91813861a6a2
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-04-18 22:00:49 -0300

    Update JavaApplication1.java

--- data-structure/class-001/exercise-002/JavaApplication1.java
+++ data-structure/class-001/exercise-002/JavaApplication1.java
@@ -4,13 +4,13 @@ public class JavaApplication1 {
     public static void main(String[] args) {
         int number = 5;
         int factorial = number;
-        String count = String.valueOf(number);
+        String account = String.valueOf(number);
         
         for (int i = number - 1; i > 0; i--) {
-            count += " x " + i;
+            account += " x " + i;
             factorial *= i;
         }
         
-        System.out.println(number + "! = " + count + " = " + factorial);
+        System.out.println(number + "! = " + account + " = " + factorial);
     }
 }
