2849b5108db4c7c360765612236fbecf074ab33a
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-04-21 14:21:40 -0300

    Update JavaApplication1.java

--- programming-technique/class-001/exercise-003/JavaApplication1.java
+++ programming-technique/class-001/exercise-003/JavaApplication1.java
@@ -5,20 +5,17 @@ import javax.swing.JOptionPane;
 
 public class JavaApplication1 {
     public static void main(String[] args) {
-        String text_1,  text_2;
-        int value_1, value_2;
-        float currency_1, currency_2;
-        
         Scanner header = new Scanner(System.in);
-        System.out.println("Digite um texto: ");
-        text_1 = header.next();
-        System.out.println("Digite um número inteiro: ");
-        value_1 = header.nextInt();
-        System.out.println("Digite um valor monetário: ");
-        currency_1 = header.nextFloat();
         
-        text_2 = JOptionPane.showInputDialog("Digite um texto: ");
-        value_2 = Integer.parseInt(JOptionPane.showInputDialog("Digite um número inteiro: "));
-        currency_2 = Float.parseFloat(JOptionPane.showInputDialog("Digite um valor monetário: "));
+        System.out.println("Digite um texto: ");
+        String firstText = header.next();
+        System.out.println("Digite um número inteiro: ");
+        int firstValue = header.nextInt();
+        System.out.println("Digite um valor monetário: ");
+        float firstCurrency = header.nextFloat();
+        
+        String secondText = JOptionPane.showInputDialog("Digite um texto: ");
+        int secondValue = Integer.parseInt(JOptionPane.showInputDialog("Digite um número inteiro: "));
+        float secondCurrency = Float.parseFloat(JOptionPane.showInputDialog("Digite um valor monetário: "));
     }
 }
