ffead9d7f3d832471448d3f718f797b196853538
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-04-25 09:41:40 -0300

    Create Main.java

---
+++ programming-technique/class-008/exercise-001/Main.java
@@ -0,0 +1,14 @@
+package javaapplication1;
+
+public class Main {
+    public static void main(String[] args) {
+        Array array = new Array(5);
+        
+        try {
+            array.add("Primeiro elemento");
+            array.add("Segundo elemento");
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+    }
+}
