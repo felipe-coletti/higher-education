fa0a3d93ede81f80b2ae56d2b811e9127e24bddb
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-04-25 11:02:40 -0300

    Update Array.java

--- programming-technique/class-008/exercise-001/Array.java
+++ programming-technique/class-008/exercise-001/Array.java
@@ -24,7 +24,7 @@ public class Array {
         return length;
     }
     
-    public void printData() {
-        System.out.println(Arrays.toString(elements));
+    public String toString() {
+        return Arrays.toString(elements);
     }
 }
