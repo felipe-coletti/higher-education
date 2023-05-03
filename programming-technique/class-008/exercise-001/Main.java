3abcd424c8201b7b12726d9ab7c9f8c34b3c8e27
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-05-03 20:54:51 -0300

    Update Main.java

--- programming-technique/class-008/exercise-001/Main.java
+++ programming-technique/class-008/exercise-001/Main.java
@@ -15,6 +15,6 @@ public class Main {
         
         System.out.println(array.getLength());
         
-        System.out.println(Arrays.toString(array.getElements()));
+        System.out.println(array.printData());
     }
 }
