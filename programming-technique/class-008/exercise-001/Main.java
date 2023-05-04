64dd24535ea61ec6406bad1792e72af9a936815d
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-05-03 21:32:53 -0300

    Update Main.java

--- programming-technique/class-008/exercise-001/Main.java
+++ programming-technique/class-008/exercise-001/Main.java
@@ -15,6 +15,8 @@ public class Main {
         
         System.out.println(array.getLength());
         
+        System.out.println(array.search(0));
+        
         System.out.println(array.printData());
     }
 }
