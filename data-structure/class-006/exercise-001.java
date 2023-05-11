f9fa9bf5cb254a424a4dbb02b56bed055fd61b1e
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-05-11 10:03:24 -0300

    Update exercise-001.java

--- data-structure/class-006/exercise-001.java
+++ data-structure/class-006/exercise-001.java
@@ -5,13 +5,11 @@ import java.util.Arrays;
 public class JavaApplication1 {
     public static void main(String[] args) {
         int vector[] = {2, 5, 1, 7, 5, 8};
-        //int smallerNumber = vector[0];
         
         for (int i = 0; i < vector.length; i++) {
             int smallerNumber = vector[i];
             int k = i;
             
-            //for (int j = 0; j < vector.length; j++) {
             for (int j = i; j < vector.length; j++) {
                 if (vector[j] < smallerNumber) {
                     smallerNumber = vector[j];
@@ -19,7 +17,6 @@ public class JavaApplication1 {
                 }
             }
             
-            //System.out.println(vector[k]);
             vector[k] = vector[i];
             vector[i] = smallerNumber;
         }
