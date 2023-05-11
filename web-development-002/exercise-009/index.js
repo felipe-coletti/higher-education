dd16126234a3ab01aafa745b27fe3fc4eb8fdf76
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-05-11 08:22:07 -0300

    Update index.js

--- web-development-002/exercise-009/index.js
+++ web-development-002/exercise-009/index.js
@@ -1,18 +1,18 @@
-const { initializeApp, applicationDefault, cert } = require('firebase-admin/app');
-const { getFirestore, Timestamp, FieldValue } = require('firebase-admin/firestore');
+const { initializeApp, applicationDefault, cert } = require("firebase-admin/app")
+const { getFirestore, Timestamp, FieldValue } = require("firebase-admin/firestore")
 
-const serviceAccount = require('');
+const serviceAccount = require("")
 
 initializeApp({
   credential: cert(serviceAccount)
-});
+})
 
-const db = getFirestore();
+const db = getFirestore()
 
 const data = {
-  name: 'name',
-  state: 'state',
-  country: 'country'
-};
+  name: "name",
+  state: "state",
+  country: "country"
+}
 
-const res = db.collection('schedules').doc('LA').set(data);
+const res = db.collection("schedules").doc("LA").set(data)
