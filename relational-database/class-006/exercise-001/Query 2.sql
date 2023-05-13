a943e571ddbc535482f09b4d5b22c0c3628c719c
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-05-13 10:13:58 -0300

    Update Query 2.sql

--- relational-database/class-006/exercise-001/Query 2.sql
+++ relational-database/class-006/exercise-001/Query 2.sql
@@ -1,7 +1,16 @@
 use college;
 
 insert into student values (1, "name", 1, "gender", 1.0);
+insert into student values (2, "name", 1, "gender", 2.0);
+insert into student values (3, "name", 1, "gender", 3.0);
 
 select * from student;
-
 select id, name, age from student where id = 1;
+select id, name, age from student where name like binary "% name";
+select id, name, age, average from student where average between 3.0 and 6.0;
+select sum(average) from student;
+select count(average) from student;
+select max(average) from student;
+select min(average) from student;
+select avg(average) from student;
+select sum(average) from student group by average;
