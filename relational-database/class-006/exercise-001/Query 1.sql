b61f2c1bb90888c044386f9e3d9b36cd9c7203d8
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-05-13 10:00:44 -0300

    Create Query 1.sql

---
+++ relational-database/class-006/exercise-001/Query 1.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `school`.`student`(
+		`id` INT NOT NULL,
+		`name` VARCHAR(255) NOT NULL,
+		`age` INT NOT NULL,
+		`gender` VARCHAR(255) NOT NULL
+) ENGINE = InnoDB;
+
+ALTER TABLE `student` ADD `average` INT NOT NULL;
