e00580e9cbb85c013eea92cf2a83d57e913b4808
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-05-13 10:00:23 -0300

    Delete Query 1.sql

--- relational-database/class-006/exercise-001/Query 1.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `college`.`student`(
-    `id` INT NOT NULL,
-    `name` VARCHAR(255) NOT NULL,
-    `age` INT NOT NULL,
-    `gender` VARCHAR(255) NOT NULL
-) ENGINE = InnoDB;
-
-ALTER TABLE `student` ADD `average` INT NOT NULL;
