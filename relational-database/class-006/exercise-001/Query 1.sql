681447975950697e5bb4f4aaf8ecee0834225b96
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-05-13 10:07:12 -0300

    Update Query 1.sql

--- relational-database/class-006/exercise-001/Query 1.sql
+++ relational-database/class-006/exercise-001/Query 1.sql
@@ -1,8 +1,9 @@
 CREATE TABLE `school`.`student`(
-		`id` INT NOT NULL,
-		`name` VARCHAR(255) NOT NULL,
-		`age` INT NOT NULL,
-		`gender` VARCHAR(255) NOT NULL
+	`id` INT NOT NULL,
+	`name` VARCHAR(255) NOT NULL,
+	`age` INT NOT NULL,
+	`gender` VARCHAR(255) NOT NULL,
+	PRIMARY KEY (`id`)
 ) ENGINE = InnoDB;
 
 ALTER TABLE `student` ADD `average` INT NOT NULL;
