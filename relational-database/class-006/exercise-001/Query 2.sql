8ab3ae2b60c1583e9ac3d715d2877847f3205d47
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-05-13 10:36:07 -0300

    Update Query 2.sql

--- relational-database/class-006/exercise-001/Query 2.sql
+++ relational-database/class-006/exercise-001/Query 2.sql
@@ -5,24 +5,45 @@ insert into student values (2, "name", 1, "gender", 2.0);
 insert into student values (3, "name", 1, "gender", 3.0);
 
 select * from student;
+
 select id, name, age from student where id = 1;
+
 select id, name, age from student where name like binary "% name";
+
 select id, name, age, average from student where average between 3.0 and 6.0;
+
 select sum(average) from student;
+
 select count(average) from student;
+
 select max(average) from student;
+
 select min(average) from student;
+
 select avg(average) from student;
+
 select sum(average) from student group by average;
 
+
+
 having avg(average) = 5;
+
 select distinct name from student;
+
 select now();
+
 select * from aluno where name is null;
+
 select upper(name) from student;
+
 select lower(name) from student;
+
 select round(average, 2) from student;
+
 select sqrt(average) from student;
+
 select truncate(average, 1) from student;
+
 select * from student order by name asc;
+
 select * from student order by name desc;
