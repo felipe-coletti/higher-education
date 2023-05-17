fd3dc38a0f1a010a730b68b3eb03033be5d80871
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-05-17 09:29:25 -0300

    Update index.js

--- web-development-002/class-010/exercise-001/index.js
+++ web-development-002/class-010/exercise-001/index.js
@@ -1,7 +1,7 @@
 const { initializeApp, applicationDefault, cert } = require("firebase-admin/app")
 const { getFirestore, Timestamp, FieldValue } = require("firebase-admin/firestore")
 
-const serviceAccount = require("")
+const serviceAccount = require("./path/to/serviceAccountKey.json")
 
 initializeApp({
   credential: cert(serviceAccount)
