2f6be1e694eacf76d94a260fb08592198256dd26
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-05-18 09:08:30 -0300

    Update JavaApplication1.java

--- data-structure/class-006/exercise-001/JavaApplication1.java
+++ data-structure/class-006/exercise-001/JavaApplication1.java
@@ -4,23 +4,23 @@ import java.util.Arrays;
 
 public class JavaApplication1 {
     public static void main(String[] args) {
-        int vector[] = {2, 5, 1, 7, 5, 8};
+        int numbers[] = {2, 5, 1, 7, 5, 8};
         
         for (int i = 0; i < vector.length; i++) {
-            int smallerNumber = vector[i];
+            int smallerNumber = numbers[i];
             int smallerNumberIndex = i;
             
-            for (int j = i; j < vector.length; j++) {
-                if (vector[j] < smallerNumber) {
-                    smallerNumber = vector[j];
+            for (int j = i; j < numbers.length; j++) {
+                if (numbers[j] < smallerNumber) {
+                    smallerNumber = numbers[j];
                     smallerNumberIndex = j;
                 }
             }
             
-            vector[smallerNumberIndex] = vector[i];
-            vector[i] = smallerNumber;
+            numbers[smallerNumberIndex] = numbers[i];
+            numbers[i] = smallerNumber;
         }
         
-        System.out.println(Arrays.toString(vector));
+        System.out.println(Arrays.toString(numbers));
     }
 }
