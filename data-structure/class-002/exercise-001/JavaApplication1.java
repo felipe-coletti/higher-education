6506bc160d13fc156e6ba430f215ee6d785e08dc
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-05-20 09:58:10 -0300

    Update JavaApplication1.java

--- data-structure/class-002/exercise-001/JavaApplication1.java
+++ data-structure/class-002/exercise-001/JavaApplication1.java
@@ -3,10 +3,10 @@ package javaapplication1;
 public class JavaApplication1 {
     public static void main(String[] args) {
         for (int line = 1; line <= 10; line++) {
-            String result = "";
+            String result = String.valueOf(line);
             
-            for (int column = 1 * line; column <= 10 * line; column += line) {
-                result += (column + " | ");
+            for (int column = 2 * line; column <= 10 * line; column += line) {
+                result += " | " + column;
             }
             
             System.out.println(result);
