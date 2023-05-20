1e91414283dcddb98beb834c161f4d962ba18ba3
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-05-20 08:44:35 -0300

    Update JavaApplication1.java

--- algorithms-and-programming-logic/class-007/exercise-006/JavaApplication1.java
+++ algorithms-and-programming-logic/class-007/exercise-006/JavaApplication1.java
@@ -14,11 +14,9 @@ public class JavaApplication1 {
             numbers[i] = header.nextInt();
         }
         
-        System.out.print("Os valores pares são: ");
-        
         for (int i = 0; i < numbers.length; i++) {
             if (numbers[i] % 2 == 0) {
-                System.out.print("O " + (i + 1) + "º número do vetor, que vale: " + numbers[i] + ".");
+                System.out.println("O " + (i + 1) + "º número é par e vale: " + numbers[i] + ".");
             }
         }
     } 
