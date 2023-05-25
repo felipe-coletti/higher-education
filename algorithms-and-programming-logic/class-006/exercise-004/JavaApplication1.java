097a8e5eca6716e34fe23914568b2f34abf1b813
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-05-25 10:46:26 -0300

    Update JavaApplication1.java

--- algorithms-and-programming-logic/class-006/exercise-004/JavaApplication1.java
+++ algorithms-and-programming-logic/class-006/exercise-004/JavaApplication1.java
@@ -2,13 +2,13 @@ package javaapplication1;
 
 public class JavaApplication1 {
     public static void main(String[] args) {
-        float price = 80;
+        float value = 80;
         int years = 1;
         
-        for (float i = price; i <= 5000; i *= 2) {
+        for (float i = value; i <= 5000; i *= 2) {
             years++;
         }
         
-        System.out.println("O preço de prestação será maior que R$ 5000,00 em " + years + " anos.");
+        System.out.println("O valor de prestação será maior que R$ 5000,00 em " + years + " anos.");
     }
 }
