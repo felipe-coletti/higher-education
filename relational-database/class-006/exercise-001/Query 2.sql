0a148c997d3705d2690883705acea53d15ef1b6e
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-05-27 19:47:24 -0300

    Update Query 2.sql

--- relational-database/class-006/exercise-001/Query 2.sql
+++ relational-database/class-006/exercise-001/Query 2.sql
@@ -14,13 +14,10 @@ select id, name, age from student where id >= 1;
 select id, name, age from student where id <= 1;
 select id, name, age from student where id >= 0 and id <= 2;
 select id, name, age from student where id = 0 or id <= 2;
-
 select id, name, age from student where name like binary 'Primeiro%';
 select id, name, age from student where name like binary '% exemplo';
 select id, name, age from student where name like binary '%Primeiro%';
-
 select id, name, age from student where name like 'Primeiro';
-
 select id, name, age from student where name in ('Primeiro', 'exemplo');
 
 select id, name, age, average from student where average between 3.0 and 6.0;
@@ -40,7 +37,6 @@ select sum(average) from student group by average;
 select avg(average) from student group by average;
 
 select gender, avg(average) from student group by gender, average;
-
 select gender, avg(average) from student group by gender, average having avg(average) = 5;
 
 select distinct name from student;
@@ -48,7 +44,6 @@ select distinct name from student;
 select now();
 
 select * from aluno where name is not null;
-
 select * from aluno where name is null;
 
 select lower(name) from student;
@@ -62,9 +57,6 @@ select sqrt(average) from student;
 select truncate(average, 1) from student;
 
 select * from student order by name asc;
-
 select * from student order by name desc;
-
 select * from student order by id asc;
-
 select * from student order by id desc;
