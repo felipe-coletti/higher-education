b57605f0a885672b6ae94f4990bc86ba2565b311
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-09-19 09:10:55 -0300

    Delete web-development-003 directory

--- web-development-003/exercise-003/src/main/java/com/fatec/produto/servico/IProdutoServico.java
+++
@@ -1,9 +0,0 @@
-package com.fatec.produto.servico;
-
-import java.util.List;
-
-import com.fatec.produto.model.Catalogo;
-
-public interface IProdutoServico {
-	public List<Catalogo> consultaCatalogo();
-}
