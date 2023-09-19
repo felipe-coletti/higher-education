af52056e0844222988eda1e65b654dfd94c5ec81
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-09-19 08:37:25 -0300

    Add files via upload

---
+++ web-development-003/exercise-002/src/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+import SeeCatalogue from "./components/seeCatalogue";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Fatec ZL</h1>
+      <SeeCatalogue />
+    </div>
+  );
+}
---
+++ web-development-003/exercise-003/src/main/java/com/fatec/produto/servico/IProdutoServico.java
@@ -0,0 +1,9 @@
+package com.fatec.produto.servico;
+
+import java.util.List;
+
+import com.fatec.produto.model.Catalogo;
+
+public interface IProdutoServico {
+	public List<Catalogo> consultaCatalogo();
+}
