751c6a805747ba7367ca7d90ebf3db8391c3bb72
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-11-08 10:25:28 -0300

    Update JavaApplication1.java

--- algorithms-and-programming-logic/class-006/exercise-002/JavaApplication1.java
+++ algorithms-and-programming-logic/class-006/exercise-002/JavaApplication1.java
@@ -6,7 +6,7 @@ public class JavaApplication1 {
         
         for (int i = 1; i <= 50; i++) {
             if (i % 5 == 0) {
-                System.out.println("O " + (j + 1) + "º multiplo de 5 maior do que 0 é: " + i);
+                System.out.println("O " + (j + 1) + "º multiplo de 5 maior do que 0 é: " + i + ".");
                 j++;
             }
         }
