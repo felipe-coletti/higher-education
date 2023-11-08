a2d8b0ede5ab3836fb55f314e29d00df5043e63d
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-11-08 09:59:52 -0300

    Update JavaApplication1.java

--- algorithms-and-programming-logic/class-006/exercise-005/JavaApplication1.java
+++ algorithms-and-programming-logic/class-006/exercise-005/JavaApplication1.java
@@ -6,13 +6,13 @@ public class JavaApplication1 {
     public static void main(String[] args) {
         Scanner header = new Scanner(System.in);
 	    
-	    int number;
+	int number;
 	    
-	    do {
-	        System.out.println("Digite um número inteiro entre 15 e 75 para continuar: ");
-	        number = header.nextInt();
-	    } while (number >= 15 && number <= 75);
+	do {
+	    System.out.println("Digite um número inteiro entre 15 e 75 para continuar: ");
+            number = header.nextInt();
+	} while (number >= 15 && number <= 75);
 	    
-	    System.out.println(number + " não é um número entre 15 e 75.");
+	System.out.println(number + " não é um número entre 15 e 75.");
     } 
 }
