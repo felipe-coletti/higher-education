45981de14610e8d6f19fde08582788d6fca389b9
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-11-08 10:14:29 -0300

    Create JavaApplication1.java

---
+++ algorithms-and-programming-logic/class-005/exercise-001/JavaApplication1.java
@@ -0,0 +1,28 @@
+package javaapplication1;
+
+import java.util.Scanner;
+
+public class JavaApplication1 {
+	public static void main(String[] args) {
+		Scanner header = new Scanner(System.in);
+		
+		int monthDaysQuantity[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30 ,31};
+	    
+	    System.out.println("Digite o número de um mês: ");
+	    int monthNumber = header.nextInt();
+	    System.out.println("Digite um ano: ");
+	    int year = header.nextInt();
+	    
+	    if (year % 4 == 0) {
+	    	monthDaysQuantity[1] += 1;
+	    }
+	    
+	    if (monthNumber >= 1 && monthNumber <= 12) {
+	    	if (monthNumber == 2) {
+	    		System.out.println("O mês " + monthNumber + " de " + year + " tem " + monthDaysQuantity[monthNumber - 1] + " dias.");
+	    	} else {
+	    		System.out.println("O mês " + monthNumber + " tem " + monthDaysQuantity[monthNumber - 1] + " dias.");
+	    	}
+	    }
+	}
+}
