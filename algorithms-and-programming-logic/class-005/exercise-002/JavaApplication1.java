04ce83063117ff4841b20fe0b34a75f0ec4de248
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-11-08 10:50:07 -0300

    Update JavaApplication1.java

--- algorithms-and-programming-logic/class-005/exercise-002/JavaApplication1.java
+++ algorithms-and-programming-logic/class-005/exercise-002/JavaApplication1.java
@@ -7,21 +7,21 @@ import java.lang.Float;
 public class JavaApplication1 {
     public static void main(String[] args) {
 	Scanner header = new Scanner(System.in);
-	    
+
 	System.out.println("Digite o valor de a: ");
         float a = header.nextFloat();
         System.out.println("Digite o valor de b: ");
         float b = header.nextFloat();
         System.out.println("Digite o valor de c: ");
         float c = header.nextFloat();
-    
+
         if (a < b + c && b < a + c && c < a + b) {
 	    if (Float.compare(a, b) == 0 && Float.compare(b, c) == 0) {
-		System.out.println("Os segmentos de reta a, b e c podem formar um triângulo equilátero.");
+                System.out.println("Os segmentos de reta a, b e c podem formar um triângulo equilátero.");
 	    } else if (Float.compare(a, b) != 0 && Float.compare(b, c) != 0) {
-		System.out.println("Os segmentos de reta a, b e c podem formar um triângulo escaleno.");
+                System.out.println("Os segmentos de reta a, b e c podem formar um triângulo escaleno.");
 	    } else {
-		System.out.println("Os segmentos de reta a, b e c podem formar um triângulo isóceles.");
+	        System.out.println("Os segmentos de reta a, b e c podem formar um triângulo isóceles.");
 	    }
         } else {
 	    System.out.println("Os segmentos de reta a, b e c não podem formar um triângulo.");
