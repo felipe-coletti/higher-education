5f5e230f0cbbeb77332b35417b27022b72269c1f
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2024-04-01 10:52:42 -0300

    Update script.js

--- first-semester/web-development-001/college-website/js/examples/example-002/script.js
+++ first-semester/web-development-001/college-website/js/examples/example-002/script.js
@@ -3,22 +3,22 @@ function validateForm() {
     let password = document.getElementById("password").value
     let confirmPassword = document.getElementById("confirm-password").value
 
-    if(cpf.length != 11) {
+    if (cpf.length != 11) {
         alert("CPF inválido.")
         register.cpf.focus()
     }
-    if(isNaN(cpf)) {
+    if (isNaN(cpf)) {
         alert("Informe apenas números para o campo CPF.")
         register.cpf.focus()
     }
-    if(confirmPassword != password) {
+    if (confirmPassword != password) {
         alert("Senhas não conferem.")
         register.confirmPassword.focus()
     }
 
     const regex = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[^\w\s]).{6,10}$/
 
-    if(!regex.exec(password)) {
+    if (!regex.exec(password)) {
         alert("A senha deve conter no mínimo uma letra maiúscula, uma letra minúscula, um número, um caractere especial, mínimo 6 e máximo 10 caracteres.")
         register.password.focus()
     }
