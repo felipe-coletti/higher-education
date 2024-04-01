d75a917d3ae83b0d6063842c7cdf02e4686820f1
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2024-04-01 10:31:31 -0300

    Update script.js

--- first-semester/web-development-001/college-website/js/examples/example-002/script.js
+++ first-semester/web-development-001/college-website/js/examples/example-002/script.js
@@ -1,30 +1,29 @@
-function validar() {
-    let cpf = cadastro.cpf.value 
-    let senha = cadastro.senha.value
-    let confirmar = cadastro.confirmar.value
+function validate() {
+    let cpf = register.cpf.value 
+    let password = register.password.value
+    let confirm = register.confirm.value
 
     if(cpf.length != 11) {
-        alert("CPF inválido")
-        cadastro.cpf.focus()
+        alert("CPF inválido.")
+        register.cpf.focus()
         return false
     }
     if(isNaN(cpf)) {
         alert("Informe apenas números para o campo CPF.")
-        cadastro.cpf.focus()
+        register.cpf.focus()
         return false
     }
-
-    if(senha != confirmar) {
+    if(confirm != password) {
         alert("Senhas não conferem.")
-        cadastro.confirmar.focus()
+        register.confirm.focus()
         return false
     }
 
     const regex = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[^\w\s]).{6,10}$/
 
-    if(!regex.exec(senha)) {
+    if(!regex.exec(password)) {
         alert("A senha deve conter no mínimo uma letra maiúscula, uma letra minúscula, um número, um caractere especial, mínimo 6 e máximo 10 caracteres.")
-        cadastro.senha.focus()
+        register.password.focus()
         return false
     }
 }
